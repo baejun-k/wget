c57c81d0df7f51a33dd91b548fd7303415c9d646
강배준 <kbjjun0707@naver.com>
2019-03-16 22:56:23 +0900

    초기 커밋.

---
+++ wget/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Net;
+
+namespace wget {
+	class Program {
+		private static readonly string HelpStr =
+@"Usage
+    wget.exe URL_ADDRESS [function] [options]
+	wget.exe file_url --overwrite false
+
+URL_ADDRESS
+    The url address to request downloads.
+
+Function
+    file   : Download the file. (default)
+    string : Download the string.
+
+Options
+    Common options of all options:
+      -u / --user     : The user of the credential.
+      -p / --password : The password of the credential.
+
+    Parameters of ""file"" options:
+      -n / --name        : The name of the file to save local.
+                           default value is the name provided by url.
+      -o / --overwrite   : Overwrite when a file with the same name exists.
+                           default value is true.
+";
+		
+		public static void PrintError(string msg) {
+			Console.Error.WriteLine(msg);
+		}
+
+		static void Main(string[] args) {
+			if(args.Length < 1) {
+				PrintError(HelpStr);
+				Environment.Exit(1);
+			}
+
+			string function = null;
+			Uri uri = null;
+			string user = null;
+			string pwd = null;
+			string dstFileName = null;
+			bool? overwrite = null;
+
+			uri = new Uri(args[0]);
+			try {
+				for (int i = 1; i < args.Length; i++) {
+					switch (args[i].ToLower()) {
+						case "string":
+							if (function == null) { function = "string"; }
+							else { PrintError(HelpStr); Environment.Exit(1); }
+							break;
+						case "file":
+							if (function == null) { function = "file"; }
+							else { PrintError(HelpStr); Environment.Exit(1); }
+							break;
+						case "-u":
+						case "--user":
+							if (user == null) { user = args[++i]; }
+							else { PrintError(HelpStr); Environment.Exit(1); }
+							break;
+						case "-p":
+						case "--password":
+							if (pwd == null) { pwd = args[++i]; }
+							else { PrintError(HelpStr); Environment.Exit(1); }
+							break;
+						case "-o":
+						case "--overwrite":
+							if (overwrite == null) { overwrite = bool.Parse(args[++i]); }
+							else { PrintError(HelpStr); Environment.Exit(1); }
+							break;
+						case "-n":
+						case "--name":
+							if (dstFileName == null) { dstFileName = args[++i]; }
+							else { PrintError(HelpStr); Environment.Exit(1); }
+							break;
+					}
+				}
+			}
+			catch (Exception) {
+				PrintError(HelpStr);
+				Environment.Exit(1);
+			}
+
+			function = function ?? "file";
+
+			NetworkCredential credential = new NetworkCredential()
+			{
+				UserName = user ?? CredentialCache.DefaultNetworkCredentials.UserName,
+				Password = pwd ?? CredentialCache.DefaultNetworkCredentials.Password
+			};
+
+			Wget.ResultCode res = Wget.ResultCode.UNKNOWN;
+			Wget.InitSecurityProtocol();
+			if (function.Equals("file")) {
+				res = Wget.GetFile(uri, dstFileName, overwrite ?? true, credential, Console.Out, Console.Error);
+			}
+			else if (function.Equals("string")) {
+				res = Wget.GetString(uri, credential, Console.Out, Console.Error);
+			}
+			Wget.Release();
+
+			Environment.ExitCode = (int)res;
+		}
+
+	}
+}
---
+++ wget/Wget.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace wget {
+	public class Wget {
+		public enum ResultCode {
+			UNKNOWN = -1,
+			COMPLETED = 0,
+			CANCELLED = 1,
+			ERROR = 2
+		}
+
+		private static SecurityProtocolType securityProtocolType;
+		public static void InitSecurityProtocol(
+			SecurityProtocolType sercurityProtocol =
+			SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls |
+			SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12)
+		{
+			securityProtocolType = ServicePointManager.SecurityProtocol;
+			ServicePointManager.SecurityProtocol = sercurityProtocol;
+		}
+
+		public static ResultCode GetString(Uri uri, NetworkCredential credential = null,
+			TextWriter outStream = null, TextWriter errStream = null)
+		{
+			Trace.Assert(uri != null);
+
+			ResultCode resCode = ResultCode.UNKNOWN;
+			credential = credential ?? CredentialCache.DefaultNetworkCredentials;
+
+			var web = new WebClient();
+			web.Credentials = credential;
+
+			web.DownloadProgressChanged += (s, e) => {
+				if (e == null) { return; }
+				errStream?.WriteLine("downloading: " + e.BytesReceived + " " + e.TotalBytesToReceive);
+			};
+			web.DownloadStringCompleted += (s, e) => {
+				if (e.Cancelled) {
+					resCode = ResultCode.CANCELLED;
+					errStream?.WriteLine("cancelled");
+				}
+				else if (e.Error != null) {
+					resCode = ResultCode.ERROR;
+					errStream?.WriteLine(e.Error.Message);
+					errStream?.WriteLine(e.Error.StackTrace);
+				}
+				else {
+					resCode = ResultCode.COMPLETED;
+					outStream?.WriteLine(e.Result);
+				}
+			};
+			try { web.DownloadStringTaskAsync(uri).Wait(); }
+			catch (Exception exc) {
+				resCode = ResultCode.ERROR;
+				errStream?.WriteLine(exc.StackTrace);
+				PrintExceptions(exc, errStream, 0);
+			}
+
+			web.Dispose();
+
+			return resCode;
+		}
+
+		public static ResultCode GetFile(Uri uri, string dstFileName, bool overWrite = true,
+			NetworkCredential credential = null,
+			TextWriter outStream = null, TextWriter errStream = null)
+		{
+			Trace.Assert(uri != null);
+
+			ResultCode resCode = ResultCode.UNKNOWN;
+			credential = credential ?? CredentialCache.DefaultNetworkCredentials;
+
+			string tmpFileName;
+			do {
+				tmpFileName = Path.Combine(
+					Path.GetDirectoryName(dstFileName??"./"),
+					Path.GetRandomFileName() + ".wgettmp");
+			} while (File.Exists(tmpFileName));
+
+			var web = new WebClient();
+			web.Credentials = credential;
+
+			web.DownloadProgressChanged += (s, e) => {
+				if (e == null) { return; }
+				errStream?.WriteLine("downloading: " + e.BytesReceived + " " + e.TotalBytesToReceive);
+			};
+			web.DownloadFileCompleted += (s, e) => {
+				if (e.Cancelled) {
+					resCode = ResultCode.CANCELLED;
+					errStream?.WriteLine("cancelled");
+				}
+				else if (e.Error != null) {
+					resCode = ResultCode.ERROR;
+					errStream?.WriteLine(e.Error.Message);
+					errStream?.WriteLine(e.Error.StackTrace);
+				}
+				else {
+					resCode = ResultCode.COMPLETED;
+				}
+			};
+
+			try { web.DownloadFileTaskAsync(uri, tmpFileName).Wait(); }
+			catch (Exception exc) {
+				resCode = ResultCode.ERROR;
+				errStream?.WriteLine(exc.StackTrace);
+				PrintExceptions(exc, errStream, 0);
+			}
+
+			if (resCode == ResultCode.COMPLETED) {
+				if (dstFileName == null) {
+					if (web.ResponseHeaders.Get("Content-Disposition") != null) {
+						string _tmp = web.ResponseHeaders.Get("Content-Disposition");
+						_tmp = _tmp.Substring(_tmp.IndexOf("filename=") + 9).Replace("\"", "");
+						dstFileName = _tmp.Split(';')[0];
+					}
+					else {
+						dstFileName = Path.GetFileName(uri.AbsolutePath);
+					}
+				}
+
+				if (!overWrite && File.Exists(dstFileName)) {
+					resCode = ResultCode.ERROR;
+					errStream?.WriteLine("exist");
+				}
+				else {
+					try {
+						if (File.Exists(dstFileName)) { File.Delete(dstFileName); }
+						File.Move(tmpFileName, dstFileName);
+					}
+					catch(Exception exc) {
+						resCode = ResultCode.ERROR;
+						errStream?.WriteLine(exc.StackTrace);
+						PrintExceptions(exc, errStream, 0);
+					}
+				}
+			}
+
+			if (File.Exists(tmpFileName)) {
+				try { File.Delete(tmpFileName); }
+				catch (Exception) { }
+			}
+
+			outStream?.WriteLine(resCode.ToString() + " " + dstFileName);
+
+			web.Dispose();
+
+			return resCode;
+		}
+
+		private static void PrintExceptions(Exception exc, TextWriter errStream, int level, int depth = -1)
+		{
+			errStream = errStream ?? Console.Error;
+			errStream.WriteLine(exc.Message);
+			if (exc.InnerException != null && (depth == -1 || level < depth)) {
+				PrintExceptions(exc.InnerException, errStream, level + 1, depth);
+			}
+		}
+
+		public static void Release()
+		{
+			ServicePointManager.SecurityProtocol = securityProtocolType;
+		}
+	}
+
+}
